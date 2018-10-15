87e3866be806a0de85e7088958fc050566a733ff
V4NC3 <nguyen.vance@gmail.com>
2018-10-15 02:24:23 -0700

    Created test case for when there are 3 numbers.  Previous test meets test req

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -14,6 +14,9 @@ namespace StringCalculator
         // Created test to return 3 when string 1 + 2.
         [TestCase(3, "1,2")]
 
+        // Created test to return 6 when string is 3 numbers.
+        [TestCase(6, "1,2,3")]
+
         public void Add_Number_ReturnSum(int expected, string number)
         {
             Assert.AreEqual(expected, Calculator.Add(number));
